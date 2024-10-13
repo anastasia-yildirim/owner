5e967c308a81af498340fedb6e89c768adab1ed1
anastasiayildirim <anastasia.skrydlova@gmail.com>
2024-10-13 21:35:43 +0300

    minor fix

--- src/test/java/config/WebDriverConfig.java
+++ src/test/java/config/WebDriverConfig.java
@@ -28,6 +28,6 @@ public interface WebDriverConfig extends Config {
     Boolean isRemote();
 
     @Key("remoteUrl")
-    @DefaultValue("http://localhost:4444")
+    @DefaultValue("https://user1:1234@selenoid.autotests.cloud/wd/hub")
     String getRemoteUrl();
 }
